265a915eb6b8ab627ae1182b524175653527722e
szj678 <66132188+szj678@users.noreply.github.com>
2021-06-05 16:31:28 +0800

    szj

---
+++ WebApplication1/WebApplication1/Administrator/administratorLogin/administratorLogin.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WebApplication1.Administrator.administratorLogin
+{
+    public partial class administratorLogin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection conn;
+            conn = new SqlConnection(@"server =.; user id = sa; password =123456; database = Web");
+            string name = Request["name"];
+            int password =Convert.ToInt32(Request["password"].ToString());//将密码转换成int
+            string sql = string.Format("SELECT password FROM administrator where userName ='{0}'", name);//对于是字符串类型的{}一定要加单引号，使其sql识别为字符串
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            conn.Open();
+            if (cmd.ExecuteScalar() == null)
+            {
+                Response.Write(-1);    //账户不存在
+                Response.End();
+            }
+            else
+            {
+                string password_sql = cmd.ExecuteScalar().ToString();
+                if (password == Convert.ToInt32(password_sql))
+                {
+                    Response.Write(1);   //账号密码正确，登录成功
+                    Response.End();
+                }
+                if (password != Convert.ToInt32(password_sql))
+                {
+                    Response.Write(0);   //密码错误
+                    Response.End();
+                }
+            }
+            conn.Close();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Administrator/administratorLogin/js/administratorLogin.js
@@ -0,0 +1,112 @@
+﻿function cookies() {
+    //var oForm = document.getElementsById("button");
+    var oUser = document.getElementById('name');
+    var oPswd = document.getElementById('pass');
+    var oRemember = document.getElementById('remember');
+    //页面初始化时，如果帐号密码cookie存在则填充
+    if (getCookie('username') && getCookie('password')) {
+        oUser.value = getCookie('username');
+        oPswd.value = getCookie('password');
+        oRemember.checked = true;
+    }
+    //复选框勾选状态发生改变时，如果未勾选则清除cookie
+    oRemember.onchange = function () {
+        if (!this.checked) {
+            delCookie('username');
+            delCookie('password');
+        }
+    };
+    //提交事件触发时，如果复选框是勾选状态则保存cookie
+    document.onclick = function () {
+        var obj = event.srcElement;
+        if (obj.type == "button") {
+            if (oRemember.checked) {
+                setCookie('username', oUser.value, 7); //保存帐号到cookie，有效期7天
+                setCookie('password', oPswd.value, 7); //保存密码到cookie，有效期7天
+            }
+        }
+    }
+}
+//设置cookie
+function setCookie(name, value, day) {
+    var date = new Date();
+    date.setDate(date.getDate() + day);
+    document.cookie = name + '=' + value + ';expires=' + date;
+};
+//获取cookie
+function getCookie(name) {
+    var reg = RegExp(name + '=([^;]+)');
+    var arr = document.cookie.match(reg);
+    if (arr) {
+        return arr[1];
+    } else {
+        return '';
+    }
+};
+//删除cookie
+function delCookie(name) {
+    setCookie(name, null, -1);
+};
+
+function pass_verify() {     //验证
+    var name = document.getElementById("name").value;
+    var password = document.getElementById("pass").value;
+    if (name == '' || password == '' || name == undefined || password == undefined || name == null || password == null)
+    {
+        alert("请输入用户名或密码！");
+        location.reload();
+    }
+    else {
+        if (isNumber(password)) {
+            submit();
+        }
+        else {
+            
+            alert("密码必须全为数字！");
+            location.reload();
+
+        }
+    }
+}
+function isNumber(value) {         //验证是否为数字
+    var patrn = /^(-)?\d+(\.\d+)?$/;
+    if (patrn.exec(value) == null || value == "") {
+        return false 
+    }
+    else {
+        return true
+    }
+    
+}
+function submit() {     //ajax传表单
+    var name = document.getElementById("name").value;
+    var password = document.getElementById("pass").value;
+    var XmlHttp = new XMLHttpRequest();
+    var form = new FormData();
+    form.append("name", name);    //创建表单
+    form.append("password", password);//创建表单
+    XmlHttp.open("Post", "administratorLogin.aspx", true);
+    XmlHttp.onreadystatechange = function () {
+        if (XmlHttp.readyState == 4 && XmlHttp.status == 200) {
+            flag = XmlHttp.responseText;
+            result(flag);
+        }
+    };   //异步
+    XmlHttp.send(form);
+}
+
+function result(flag) {
+    if (flag == 1) {
+        alert("登陆成功！");
+        window.location.href = "#"; //跳转到管理员操作界面
+    }
+    else if (flag == -1) {
+        alert("账户不存在!");
+        location.reload();
+    }
+    else {
+        alert("账户或密码错误！");
+        location.reload();
+    }
+}
+
